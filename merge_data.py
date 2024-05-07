7d752bc8f5acf42d397948bff81de4f28bced188
jinbeom, jeong <ppgt1225@naver.com>
2024-05-07 16:00:50 +0900

    fix merge data app

---
+++ merge_data.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import freeze_support
+from tqdm import tqdm
+
+
+data_root_path = "d:\\joystick"
+sample_name = 'RAW DATA 1'
+
+file_path_list = list()
+
+for file in os.listdir(os.path.join(data_root_path, sample_name, 'Durability')):
+    if file.endswith('.CSV'):
+        file_path_list.append(os.path.join(data_root_path, sample_name, 'Durability', file))
+
+file_path_list.sort()
+
+data_column_names = ['port_1_p(bar)', 'port_2_p(bar)', 'port_3_p(bar)', 'port_4_p(bar)', 'support_p(bar)', 'tank_p(bar)', 'temp(c)']
+float32_target_var = ['port_1_p(bar)', 'port_2_p(bar)', 'port_3_p(bar)', 'port_4_p(bar)','support_p(bar)', 'tank_p(bar)', 'time(hour)']
+uint8_target_var = ['temp(c)']
+
+column_types = {'port_1_p(bar)': 'float32', 'port_2_p(bar)': 'float32', 'port_3_p(bar)': 'float32',
+                'port_4_p(bar)': 'float32', 'support_p(bar)': 'float32', 'tank_p(bar)': 'float32', 'time(hour)': 'float32', 'temp(c)': 'float32'}
+
+
+def load_csv(path_list):
+    data = pd.read_csv(path_list, encoding='cp949').iloc[9:]
+    data.columns = data_column_names
+
+    return data
+
+
+def load_csv_executor(path_list):
+    with ProcessPoolExecutor(max_workers=int(os.cpu_count())) as executor:
+        df_list = list(tqdm(executor.map(load_csv, path_list), desc='dataframe loading...', total=len(path_list)))
+
+    return pd.concat(df_list)
+
+
+if __name__ == '__main__':
+    freeze_support()
+
+    result_df = load_csv_executor(file_path_list)
+    result_df['time(hour)'] = np.arange(result_df.shape[0]) / (100 * 3600)
+    result_df.reset_index(inplace=True, drop=True)
+
+    for var in tqdm(float32_target_var, desc='float32 converting...'):
+        result_df[var] = result_df[var].astype(np.float32)
+
+    for var in tqdm(uint8_target_var, desc='uint8 converting...'):
+        result_df[var] = result_df[var].astype(np.float32).astype(np.uint8)
+
+    result_df.to_parquet('raw_data_1.parquet')
+
+    print(result_df.info())
